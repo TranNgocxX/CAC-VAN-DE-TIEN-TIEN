c4ad6cfe575be385b0e681f82c7f815abf81c561
TranNgocxX <152256291+TranNgocxX@users.noreply.github.com>
2025-06-18 01:53:44 +0700

    Update QuanLySanPhamFacade.java

--- BT/src/main/java/BTL/patterns/QuanLySanPhamFacade.java
+++ BT/src/main/java/BTL/patterns/QuanLySanPhamFacade.java
@@ -1,17 +1,3 @@
-//package BTL.patterns;
-//
-//import java.util.List;
-//import BTL.daos.SanPhamDAO;
-//import BTL.models.SanPham;
-//
-//public class QuanLySanPhamFacade {
-//    private SanPhamDAO sanPhamDAO = new SanPhamDAO();
-//
-//    public List<SanPham> layTatCaSanPham() {
-//        return sanPhamDAO.layTatCa();
-//    }
-//}
-
 package BTL.patterns;
 
 import BTL.daos.SanPhamDAO;
