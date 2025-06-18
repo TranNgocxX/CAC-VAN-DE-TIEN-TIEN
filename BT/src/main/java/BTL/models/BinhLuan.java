608dafc11d97442d158c6e7ee9a4a180ff28ee16
TranNgocxX <152256291+TranNgocxX@users.noreply.github.com>
2025-06-18 22:57:32 +0700

    Update BinhLuan.java

--- BT/src/main/java/BTL/models/BinhLuan.java
+++ BT/src/main/java/BTL/models/BinhLuan.java
@@ -1,71 +1,38 @@
 package BTL.models;
 
-import java.util.Date;
+import java.sql.Timestamp;
 
 public class BinhLuan {
     private int id;
     private int idNguoiDung;
     private int idSanPham;
     private String noiDung;
-    private Date ngayTao;
-    private Date ngayCapNhat;
-    
-    // Constructor, getter và setter
+    private Timestamp ngayTao;
+    private Timestamp ngayCapNhat;
+
+    private NguoiDung nguoiDung; 
+
     public BinhLuan() {}
     
-    public BinhLuan(int idNguoiDung, int idSanPham, String noiDung) {
-        this.idNguoiDung = idNguoiDung;
-        this.idSanPham = idSanPham;
-        this.noiDung = noiDung;
-        this.ngayTao = new Date();
-        this.ngayCapNhat = new Date();
-    }
-    
-    public int getId() {
-        return id;
-    }
-    
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    public int getIdNguoiDung() {
-        return idNguoiDung;
-    }
-    
-    public void setIdNguoiDung(int idNguoiDung) {
-        this.idNguoiDung = idNguoiDung;
-    }
-    
-    public int getIdSanPham() {
-        return idSanPham;
-    }
-    
-    public void setIdSanPham(int idSanPham) {
-        this.idSanPham = idSanPham;
-    }
-    
-    public String getNoiDung() {
-        return noiDung;
-    }
-    
-    public void setNoiDung(String noiDung) {
-        this.noiDung = noiDung;
-    }
-    
-    public Date getNgayTao() {
-        return ngayTao;
-    }
-    
-    public void setNgayTao(Date ngayTao) {
-        this.ngayTao = ngayTao;
-    }
-    
-    public Date getNgayCapNhat() {
-        return ngayCapNhat;
-    }
-    
-    public void setNgayCapNhat(Date ngayCapNhat) {
-        this.ngayCapNhat = ngayCapNhat;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getIdNguoiDung() { return idNguoiDung; }
+    public void setIdNguoiDung(int idNguoiDung) { this.idNguoiDung = idNguoiDung; }
+
+    public int getIdSanPham() { return idSanPham; }
+    public void setIdSanPham(int idSanPham) { this.idSanPham = idSanPham; }
+
+    public String getNoiDung() { return noiDung; }
+    public void setNoiDung(String noiDung) { this.noiDung = noiDung; }
+
+    public Timestamp getNgayTao() { return ngayTao; }
+    public void setNgayTao(Timestamp ngayTao) { this.ngayTao = ngayTao; }
+
+    public Timestamp getNgayCapNhat() { return ngayCapNhat; }
+    public void setNgayCapNhat(Timestamp ngayCapNhat) { this.ngayCapNhat = ngayCapNhat; }
+
+    public NguoiDung getNguoiDung() { return nguoiDung; }
+    public void setNguoiDung(NguoiDung nguoiDung) { this.nguoiDung = nguoiDung; }
 }
+
